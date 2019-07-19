4b9883f50c35e96cf4cf47b026c6b59637f850ea
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-19 17:46:31 +0300

    editItem added

--- app/client/views/item/createItem.js
+++ app/client/views/item/createItem.js
@@ -12,19 +12,22 @@ const createItem = Vue.component('create-item', {
 
   methods: {
     onSubmit: function(){
-      console.log(this.item);
-      axios.post('/item/create', this.item)
-      .then((response) => {
-        console.log(response);
+      fetch('/item', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(this.item)
       })
-      .catch(function (error) {
-        console.log(error);
-      });
-      this.item = {
-        creator: '',
-        title: '',
-        subjects: ''
-      };
+      .then(response => {
+        return response.text();
+      })
+      .then(res => {
+        console.log(res);
+        this.item = {
+          creator: '',
+          title: '',
+          subjects: ''
+        }
+      })
     }
   },
 
@@ -34,18 +37,53 @@ const createItem = Vue.component('create-item', {
           <div class = "col-12">
             <div class="display-4 text-muted text-center">Create a new item</div>
             <hr>
+            <div class= "row justify-content-center">
+              <div class = 'form-group'
+                <div class ='col-md-12'>
+                  <div class = 'row'>
 
-            <div class = 'form-group'
-            <label for="creator">Creator</label>
-            <input type="text" class="form-control mb-3" v-model="item.creator" id = "creator" name = "creator" placeholder="Type name..">
+                    <div class = 'col-md-6'>
+                      <label for="creator">Creator</label>
+                      <input type="text" class="form-control mb-3" v-model="item.creator" id = "creator" name = "creator" placeholder="Type name..">
 
-            <label for="title">Title</label>
-            <input type="text" class="form-control mb-3" v-model="item.title" id = "title" name = "title" placeholder="Type title.. ">
+                      <label for="title">Title</label>
+                      <input type="text" class="form-control mb-3" v-model="item.title" id = "title" name = "title" placeholder="Type title.. ">
 
-            <label for="subjects">Few words about</label>
-            <textarea class="form-control mb-3" id="subjects" v-model="item.subjects" name="subjects" rows="3"></textarea>
+                      <label for="usageClass">UsageClass</label>
+                      <input type="text" class="form-control mb-3" v-model="item.usageClass" id = "usageClass" name = "usageClass" placeholder="Type usage class.. ">
+                    </div>
+
+                    <div class = 'col-md-6'>
+                      <label for="checkoutType">CheckoutType</label>
+                      <input type="text" class="form-control mb-3" v-model="item.checkoutType" id = "checkoutType" name = "checkoutType" placeholder="..">
+    
+                      <label for="materialType">MaterialType</label>
+                      <input type="text" class="form-control mb-3" v-model="item.materialType" id = "materialType" name = "materialType" placeholder="..">
+                      
+                      <label for="publisher">Publisher</label>
+                      <input type="text" class="form-control mb-3" v-model="item.publisher" id = "publisher" name = "publisher" placeholder="..">
+    
+                      <label for="publicationYear">PublicationYear</label>
+                      <input type="text" class="form-control mb-3" v-model="item.publicationYear" id = "publicationYear" name = "publicationYear" placeholder="..">
+                    </div>
+                  </div>
+                </div>
+                <div class = 'col-md-12'>
+                  <label for="checkoutYear">CheckoutYear</label>
+                  <input type="text" class="form-control mb-3" v-model="item.checkoutYear" id = "checkoutYear" name = "checkoutYear" placeholder="..">
+
+                  <label for="checkoutMonth">CheckoutMonth</label>
+                  <input type="text" class="form-control mb-3" v-model="item.checkoutMonth" id = "checkoutMonth" name = "checkoutMonth" placeholder="..">
+
+                  <label for="checkouts">Checkouts</label>
+                  <input type="text" class="form-control mb-3" v-model="item.checkouts" id = "checkouts" name = "checkouts" placeholder="..">
+                  
+                  <label for="subjects">Few words about</label>
+                  <textarea class="form-control mb-3" id="subjects" v-model="item.subjects" name="subjects" rows="3"></textarea>
+                </div>
+              </div>
+              <button class="btn btn-primary mb-5" type="submit">Create</button>
             </div>
-            <button class="btn btn-primary mb-5" type="submit">Create</button>
         </div>
       </div>
     </form>
--- app/client/views/item/getItem.js
+++ app/client/views/item/getItem.js
@@ -8,42 +8,100 @@ const getItem = Vue.component('getItem',{
         this.item = response;
         console.log(this.item);
       })
+
+      //console.log(`Parametrul din query este : ${this.$route.query.edit}`);
+      this.edit = this.$route.query.edit;
+      console.log(this.edit);
   },
   data: function () {
     return {
-      item: {
-
-      }
+      item: { },
+      edit: 'false'
     }
   }, 
+  methods: {
+    startEdit(){
+      this.edit = 'true';
+      console.log(this.edit);
+      this.$route.query.edit = 'true';
+    },
+    saveEdits() {
+      this.edit = 'false';
+      console.log(this.edit);
+      this.$route.query.edit = 'false';
+    }
+  },
   template: 
   `
   <div class = 'row justify-content-center m-3'>
     <div class = 'card' style="width: 30rem;">
-      <p class = 'text-muted text-center card-header'> {{ item.Title }} </p>
-      <small class = 'text-center'><strong>by {{ item.Creator }}</strong></small>
 
-      <p class = 'font-italic text-left card-body'> {{ item.Subjects }} </p>
+      <p class = 'text-muted text-center card-header' v-if="edit == 'false'"> {{ item.Title }} </p>
+      <input type='text' class = 'text-muted text-center card-header' v-else="edit == 'true'" value = [item.Title] />
+      
+      <small class = 'text-center' v-if="edit == 'false'"><strong>by <u>{{ item.Creator }} </u></strong></small>
+      <input type= 'text' class = 'text-center' value = '' v-else="edit == 'true'"/>
+
+      <p class = 'font-italic text-left card-body' v-if="edit == 'false'"> {{ item.Subjects }} </p>
+      <input type = 'text' class = 'font-italic text-left card-body' value = '' v-else="edit == 'true'" />
 
       <div class = 'card-footer'>
         <div class = 'row'>
 
           <div class = 'col col-md-6'>
-            <div><small>MaterialType: <span class = 'text-muted font-italic'>{{ item.MaterialType }} </span></small></div>
-            <div><small>UsageClass: <span class = 'text-muted font-italic'>{{ item.UsageClass }} </span></small></div>
-            <div><small>CheckoutType: <span class = 'text-muted font-italic'>{{ item.CheckoutType }} </span></small></div>
-            <div><small>CheckoutYear: <span class = 'text-muted font-italic'>{{ item.CheckoutYear }} </span></small></div>
+          
+            <div>MaterialType: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.MaterialType }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>UsageClass: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.UsageClass }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>CheckoutType: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.CheckoutType }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>CheckoutYear: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.CheckoutYear }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+            
           </div>
 
           <div class = 'col col-md-6'>
-            <div><small>CheckoutMonth: <span class = 'text-muted font-italic'>{{ item.CheckoutMonth }} </span></small></div>
-            <div><small>Checkouts: <span class = 'text-muted font-italic'>{{ item.Checkouts }} </span></small></div>
-            <div><small>Publisher: <span class = 'text-muted font-italic'>{{ item.Publisher }} </span></small></div>
-            <div><small>PublicationYear: <span class = 'text-muted'>{{ item.PublicationYear }} </span></small></div>
+
+            <div>CheckoutMonth: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.CheckoutMonth }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>Checkouts: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.Checkouts }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>Publisher: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.Publisher }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
+
+            <div>PublicationYear: 
+              <span class = 'text-muted font-italic' v-if="edit == 'false'">{{ item.PublicationYear }} </span>
+              <input type = 'text' class = 'text-muted font-italic' v-else="edit == 'true'" value = '' />
+            </div>
           </div>
         
       </div>
+      <div class = 'row'>
+        <input class = 'btn btn-sm btn-edit mt-4' @click='startEdit' v-if="edit == 'false'" value="Edit">
+        <input class = 'btn btn-sm btn-edit mt-4' @click='saveEdits' v-else="edit == 'true'" value="Save">
+      </div>
     </div>
+  </div>
   </div>
   `
 })

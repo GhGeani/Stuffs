17f0e877b1aa0dabf73cefee2987a8e03f51c39d
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-19 13:32:39 +0300

    getItem component

--- app/client/views/item/getItem.js
+++ app/client/views/item/getItem.js
@@ -1,20 +1,48 @@
 const getItem = Vue.component('getItem',{
+  created: function() {
+    fetch(`/item/${this.$route.params.id}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        this.item = response;
+        console.log(this.item);
+      })
+  },
   data: function () {
     return {
       item: {
 
       }
     }
-  },
-
+  }, 
   template: 
   `
-  <div class = 'row'>
-    <div class = 'card col-md-5 col-lg-3 item m-3' style="width: 20rem;">
-      <p class = 'text-muted text-center'> {{ item.title }} </p>
-      <small><strong>by {{ item.creator }}</strong></small>
-      <hr>
-      <p class = 'font-italic text-left'> {{ item.subjects }} </p>
+  <div class = 'row justify-content-center m-3'>
+    <div class = 'card' style="width: 30rem;">
+      <p class = 'text-muted text-center card-header'> {{ item.Title }} </p>
+      <small class = 'text-center'><strong>by {{ item.Creator }}</strong></small>
+
+      <p class = 'font-italic text-left card-body'> {{ item.Subjects }} </p>
+
+      <div class = 'card-footer'>
+        <div class = 'row'>
+
+          <div class = 'col col-md-6'>
+            <div><small>MaterialType: <span class = 'text-muted font-italic'>{{ item.MaterialType }} </span></small></div>
+            <div><small>UsageClass: <span class = 'text-muted font-italic'>{{ item.UsageClass }} </span></small></div>
+            <div><small>CheckoutType: <span class = 'text-muted font-italic'>{{ item.CheckoutType }} </span></small></div>
+            <div><small>CheckoutYear: <span class = 'text-muted font-italic'>{{ item.CheckoutYear }} </span></small></div>
+          </div>
+
+          <div class = 'col col-md-6'>
+            <div><small>CheckoutMonth: <span class = 'text-muted font-italic'>{{ item.CheckoutMonth }} </span></small></div>
+            <div><small>Checkouts: <span class = 'text-muted font-italic'>{{ item.Checkouts }} </span></small></div>
+            <div><small>Publisher: <span class = 'text-muted font-italic'>{{ item.Publisher }} </span></small></div>
+            <div><small>PublicationYear: <span class = 'text-muted'>{{ item.PublicationYear }} </span></small></div>
+          </div>
+        
+      </div>
     </div>
   </div>
   `

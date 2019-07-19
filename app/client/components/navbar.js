4ee69c8a2698f9bbbfde2d5730a23d8ea2f0b1b1
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-19 17:45:53 +0300

    small changes

--- app/client/components/navbar.js
+++ app/client/components/navbar.js
@@ -2,10 +2,10 @@ Vue.component('nav-bar', {
 
   template: `
   <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-  <router-link class="navbar-brand" to="/">List Of Items</router-link>
+  <router-link class="navbar-brand" to="/items">List Of Items</router-link>
     <ul class="navbar-nav float-right">
       <li class="nav-item">
-        <router-link to="/item/create" class="nav-link">Create</router-link>
+        <router-link to="/item" class="nav-link">Create</router-link>
       </li>
     </ul>
 </nav>
--- app/client/views/item/item.js
+++ app/client/views/item/item.js
@@ -7,11 +7,11 @@ Vue.component('item', {
   <div class = "item card col-md-5 col-lg-3 m-3">
     <div class = 'card-header'>
       <p class = 'text-muted text-center'> {{ title }} </p>
-      <small><strong>by {{ creator }}</strong></small>
+      <small><strong>by <u>{{ creator }} </u></strong></small>
     </div>
     <p class = 'card-body text-truncate'> {{ subjects }} </p>
-    <router-link class ='btn btn-sm btn-get-more' :to="'item/' + id">Get more</router-link>
-    <router-link class ='btn btn-sm btn-edit' :to="'item/edit/' + id">Edit</router-link>
+    <router-link class ='btn btn-sm btn-get-more' :to="{ path: 'item/' + id , query: { edit: 'false' } }">Get more</router-link>
+    <router-link class ='btn btn-sm btn-edit' :to="{ path: 'item/' + id, query: { edit: 'true' } }">Edit</router-link>
     <button class ='btn btn-sm btn-del' @click="remove">Delete</button>
   </div>
   `,

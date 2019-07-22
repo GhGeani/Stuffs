b137527692708604c4fe6c9ce93138c872b27197
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-22 11:36:14 +0300

    confirm delete

--- app/client/views/item/item.js
+++ app/client/views/item/item.js
@@ -12,22 +12,28 @@ Vue.component('item', {
     <p class = 'card-body text-truncate'> {{ subjects }} </p>
     <router-link class ='btn btn-sm btn-get-more' :to="{ path: 'item/' + id , query: { edit: 'false' } }">Get more</router-link>
     <router-link class ='btn btn-sm btn-edit' :to="{ path: 'item/' + id, query: { edit: 'true' } }">Edit</router-link>
-    <button class ='btn btn-sm btn-del' @click="remove">Delete</button>
+    <input type = 'button' class ='btn btn-sm btn-del' :id = id @click="remove" value='Delete' />
   </div>
   `,
 
   methods: {
     remove() {
-      fetch(`/item/${this.id}`, {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},
-      })
-      .then(res => {
-        return res.text();
-      })
-      .then(res => {
-        console.log(res);
-        this.$parent.$emit('item:remove', this.id);
+      let button = document.getElementById(this.id);
+      button.classList.remove('btn-del');
+      button.classList.add('confirm-delete');
+      button.value = 'Are you sure you want this?';
+      button.addEventListener('click', ()=>{
+        fetch(`/item/${this.id}`, {
+          method: 'DELETE',
+          headers: {'Content-Type': 'application/json'},
+        })
+        .then(res => {
+          return res.text();
+        })
+        .then(res => {
+          console.log(res);
+          this.$parent.$emit('item:remove', this.id);
+        })
       })
     }
   }

f59c7823f07197708623ef230bc3c1ecd38960ba
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-22 18:27:11 +0300

    filter

---
+++ app/client/components/filterComp.js
@@ -0,0 +1,22 @@
+const filterComp = Vue.component('filter-comp', {
+  props: [],
+
+  template: `
+    <div class="row">
+      <div class="card">
+        <div class="card-header text-center text-muted">Filters</div>
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item">
+              <span>Name</span>
+              <input type="text" class="form-control" />
+            </li>
+          </ul>
+        </div>
+        <button class="btn btn-sm btn-block btn-search">
+          Search
+        </button>
+      </div>
+    </div>
+  `
+})

0de0d60662f11b9d34a698bbb12bd56d93f86088
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-22 18:28:12 +0300

    searchItem

---
+++ app/client/components/searchComp.js
@@ -0,0 +1,9 @@
+const search = Vue.component('search', {
+  
+
+  template: `
+    <div class="row">
+        <input class="form-control" type="text" placeholder="Search by title.." aria-label="Search">
+    </div>
+  `
+})

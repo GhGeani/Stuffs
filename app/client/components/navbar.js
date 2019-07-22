becbf6079130fd448adb54357a17ffea6ad13dc1
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-22 18:27:23 +0300

    navBar changes

--- app/client/components/navbar.js
+++ app/client/components/navbar.js
@@ -2,12 +2,13 @@ Vue.component('nav-bar', {
 
   template: `
   <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-  <router-link class="navbar-brand" to="/items">List Of Items</router-link>
-    <ul class="navbar-nav float-right">
+    <router-link class="navbar-brand" to="/authors">Authors</router-link>
+    <router-link class="navbar-brand" to="/items">List Of Items</router-link>
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <router-link to="/item" class="nav-link">Create</router-link>
       </li>
     </ul>
-</nav>
+  </nav>
   `
 })

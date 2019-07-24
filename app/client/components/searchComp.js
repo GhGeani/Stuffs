3f7f75e124fea982364128aba73d3f6ab3108902
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-24 18:02:31 +0300

    reusable i think..

--- app/client/components/searchComp.js
+++ app/client/components/searchComp.js
@@ -1,9 +1,40 @@
 const search = Vue.component('search', {
-  
+  props: [
+    'path',
+    'currPage',
+    'query',
+    'url'
+  ],
+  data: function() {
+    return {
+      search: ''
+    }
+  },
+
+  methods: {
+    keymonitor() {
+      //console.log(this.search);
+      if(this.search.trim() == '') {
+        router.push({ 
+          path: this.path,
+          query: {
+            page: this.currPage
+          }
+        })
+      } else {
+        router.push({ 
+          path: this.url,
+          query: {
+            [this.query]: this.search
+          }
+        })
+      }
+    }
+  },
 
   template: `
     <div class="row">
-        <input class="form-control" type="text" placeholder="Search by title.." aria-label="Search">
+        <input class="form-control" type="text" placeholder="Search by title.." v-on:keyup="keymonitor" v-model="search">
     </div>
   `
 })

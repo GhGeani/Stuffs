4af12997f0d4a983838f185b8fedf3fc576f6a89
Gherghe Geani <GGeani@AI-WEB3.caphyon.net>
2019-07-24 18:03:04 +0300

    new routes for item publisher and author

--- app/client/routes.js
+++ app/client/routes.js
@@ -1,9 +1,11 @@
 const routes = [
   { path: '/items', component: itemList },
+  { path: '/items/search', component: itemSearch },
   { path: '/item', component: createItem },
   { path: '/item/:id', component: getItem},
   { path: '/authors', component: authorList},
-  { path: '/publishers', component: publisherList}
+  { path: '/publishers', component: publisherList},
+  { path: '/author/:name', component: getAuthor }
 ];
 
 const router = new VueRouter({
